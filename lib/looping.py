631fa94c62d11c760fbc8b453da5e7948959c641
luis <servinluis21@yahoo.com>
2024-01-01 19:03:36 -0500

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+
+    print("Happy New Year!")
+
+happy_new_year()
+   
+
+def square_integers(numbers):
+    squared_numbers = []
+
+    for num in numbers:
+        squared_numbers.append(num ** 2)
+
+    return squared_numbers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
